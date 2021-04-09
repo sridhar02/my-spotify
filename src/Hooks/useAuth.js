cbef646d931d99a919bc2900f4b2b82392d1c045
sridhar02 <kattasridhar02@gmail.com>
2021-04-10 01:03:58 +0530

    auth hook

---
+++ src/Hooks/useAuth.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+export default function useAuth(code) {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+
+  useEffect(() => {
+    if (!code) return;
+    axios
+      .post('http://localhost:3001/login', { code })
+      .then((res) => {
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, '/');
+      })
+      .catch(() => {
+        window.location = '/';
+      });
+  }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const timeout = setInterval(() => {
+      axios
+        .post('http://localhost:3001/refresh', { refreshToken })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location = '/';
+        });
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearTimeout(timeout);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
+}
