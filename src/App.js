f03d08cc5accfd0c40fd36228b8951be4b108726
sridhar02 <kattasridhar02@gmail.com>
2021-04-10 01:03:11 +0530

    main app components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+
+const code = new URLSearchParams(window.location.search).get("code");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>{code ? <Dashboard code={code} /> : <Login />}</div>;
 }
 
 export default App;
